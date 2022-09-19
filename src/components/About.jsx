4fd474c1314da746dd58c8b54911b9a2dd6a99bb
eddykungu <edkungu@gmail.com>
2022-09-19 10:18:27 +0300

    Adding a better narration.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,8 +14,9 @@ const About = () => {
         <Typography>&nbsp;</Typography>
         <Typography component='p'>Once I completed my certification at e-Mobilis, I set out to do personal projects that helped me improve my programming skills. After a while I joined campus for my bachelor's degree, Bachelor of Science in Mathematics and Computer Science at <a href="https://www.jkuat.ac.ke/">Jomo Kenyatta University of Agriculture and Technology</a>, where I took <i>pure mathematics</i> as a minor. This helped me majorly in understandingthe though process of coming up with a solution through algorithms. Through various projects and assignments in university, my skills improved tremendously.</Typography>
         <Typography>&nbsp;</Typography>
-        <Typography component='p'>I am always looking forward to being innovative while creating ways of writing the most effective code. At times, I will find ways of automating tasks so as to improve the quality of work done while reducing the time taken to do it.
-      </Typography>
+        <Typography component='p'>Since my sofware engineering started in 2018, I have primarily worked on 
+        android application development. That has help me gain tremendously. For the 4 years, I have majorly used Java but recently I have used kotlin for around 1 year. The change to kotlin was not that bug a hurdle due to prior Java knowledge.
+         I have extensive knowledge in android sdk, and moreso most crutial libraries including dagger-hilt, ktor, room and reactive programming especially using kotlin coroutines.</Typography>
     </Box>
     
     
